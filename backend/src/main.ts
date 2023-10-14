0c4b8806c18ea9f401489cd04e196c57a19b0ee1
Mikhail Toropchinov <ruhose73@gmail.com>
2023-10-14 11:47:33 +0300

    ci(docker): update docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
